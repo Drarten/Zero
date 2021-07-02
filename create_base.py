ab63211cb5a52b17b785aa4f51065e71649797de
Drarten <blood.elf22@mail.ru>
2021-07-02 22:12:02 +0300

    memo+2

--- create_base.py
+++ create_base.py
@@ -4,10 +4,19 @@ import sqlite3 as sq
 connection = sq.connect('base.db')
 cursor = connection.cursor()
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS Test(
-	name TEXT,
-	age INT,
-	day INT
+cursor.execute("""CREATE TABLE IF NOT EXISTS games(
+	user_id INT,
+	score INT,
+	'time' INT
 	)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS users(
+	rowid INT PRIMARY KEY,
+	name TEXT,
+	sex INT,
+	old INT,
+	score INT
+	)""")
+
 
 connection.close()
--- work_with_base.py
+++ work_with_base.py
@@ -4,5 +4,25 @@ import sqlite3 as sq
 connection = sq.connect('base.db')
 cursor = connection.cursor()
 
-cursor.execute("INSERT INTO Test VALUES ('Арсений', '19', '1')")
+cursor.execute("SELECT count(age) as Возраст FROM Test WHERE day = 1")
 connection.commit()
+
+
+
+
+
+
+
+
+
+
+
+
+# import sqlite3 as sq
+
+
+# connection = sq.connect('base.db')
+# cursor = connection.cursor()
+
+# cursor.execute("INSERT INTO Test VALUES ('Арсений', '19', '1')")
+# connection.commit()
