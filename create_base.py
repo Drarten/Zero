1cfe2330eb4104cd3fb146a9a283555cbd84fa9e
Drarten <blood.elf22@mail.ru>
2021-07-12 16:36:56 +0300

    s

--- create_base.py
+++ create_base.py
@@ -4,19 +4,21 @@ import sqlite3 as sq
 connection = sq.connect('base.db')
 cursor = connection.cursor()
 
-class Aut:
-	cursor.execute("""CREATE TABLE IF NOT EXISTS authorize(
-		id INT,
-		user_name TEXT,
-		token TEXT
-		)""")
 
-class Con:
-	cursor.execute("""CREATE TABLE IF NOT EXISTS content(
-		id INT,
-		title TEXT,
-		content TEXT
-		)""")
+class table:
+	def aut(bool1):
+		if bool1:
+			cursor.execute("CREATE TABLE IF NOT EXISTS authorize(id INT, user_name TEXT, token TEXT)")
+
+	def con(bool2):
+		if bool2:
+			cursor.execute("CREATE TABLE IF NOT EXISTS content(id INT, title TEXT, content TEXT)")
+
+
+table.aut(cursor.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='authorize'"))
+table.con(cursor.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='content'"))
 
 connection.close()
 #В стандартном дистрибутиве SQLite невозможно установить пароль для базы данных SQLite.
+#Прогуглив что-либо про yaml я нихуя не понял
+
